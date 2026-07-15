cd5940d173f003734999e93e02ec6eb9d5ccbdde
Mickael <mickael.contact@gmail.com>
2026-07-15 21:56:24 +0200

    changement nouveau balise h2

--- index.php
+++ index.php
@@ -10,6 +10,6 @@
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eligendi iure corporis aliquid molestias odio error beatae tempora! Reprehenderit dolorem voluptates unde, labore deleniti voluptas adipisci id doloremque consectetur enim.
     Facilis sequi distinctio illum eaque a voluptas error, maxime nisi? Voluptate obcaecati dicta officiis aliquam incidunt quod voluptas et dignissimos id animi, aliquid architecto ea assumenda magnam, repudiandae vitae ipsa.
     Iure, ad totam. Numquam, deserunt earum obcaecati officia atque non harum ab. Sapiente libero ipsum, deleniti repellendus nisi dicta quisquam necessitatibus sunt praesentium ratione vitae amet soluta voluptatem, culpa pariatur.</p>
-    
+    <h2>CIAO</h2>
 </body>
 </html>
